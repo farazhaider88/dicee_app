f14b0d1d94bd30c83605ee1af9b0d5cb6912f5ea
farazhaider88 <farazhaider88@yahoo.com>
2020-03-13 00:37:20 +0500

    Update main.dart
    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Diceeeeee'),
           backgroundColor: Colors.yellow,
         ),
         body: DicePage(),
