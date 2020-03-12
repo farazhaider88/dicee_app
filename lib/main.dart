2aa958ccae5a42ffbe678f514bd44b46ecb221fc
FarazHaider <farazhaider88@yahoo.com>
2020-03-12 20:19:59 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.yellow,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                child: Image.asset(
+                  'images/dice1.png',
+                ),
+                onPressed: () {
+                  print('left Button Pressed');
+                }),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              child: Image(
+                image: AssetImage(
+                  'images/dice1.png',
+                ),
+              ),
+              onPressed: () {
+                print('Right Button Pressed');
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
